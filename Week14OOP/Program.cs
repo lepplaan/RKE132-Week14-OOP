4e4fe85f120c1edf10f71c41710e93fd93dc07cb
Toomas Lepplaan <lepplaan@gmail.com>
2024-12-04 15:30:27 +0200

    Class, getter, method

---
+++ Week14OOP/Program.cs
@@ -0,0 +1,60 @@
+﻿Console.WriteLine("Name your dog:");
+string myDogName = Console.ReadLine();
+Dog myDog = new Dog(myDogName);
+
+Dog neighboursDog = new Dog("Good Girl");
+
+Console.WriteLine($"My dog's name is {myDog.Name}.");
+Console.WriteLine($"My neighbour's dog's name is {neighboursDog.Name}.");
+
+myDog.Rename("Bad Boy");
+
+while (myDog.LevelOfHappiness != 5)
+{
+    myDog.Bark();
+}
+
+myDog.WagTail();
+
+
+
+class Dog
+{
+    private string _name; //field
+    private int _levelOfHappiness;
+
+    //constructor
+    public Dog(string name) //name - let the user name the dog
+    {
+        _name = name;
+        _levelOfHappiness = 0;
+    }
+
+    //getter
+    public string Name
+    {
+        get { return _name; }
+    }
+
+    public void Rename(string newName)
+    {
+        _name = newName;
+        Console.WriteLine($"The dog has been renamed to {newName}.");
+    }
+
+    public int LevelOfHappiness
+    {
+        get { return _levelOfHappiness; }
+    }
+
+    public void Bark()
+    {
+        Console.WriteLine("Woof-woof");
+        _levelOfHappiness++;
+    }
+
+    public void WagTail()
+    {
+        Console.WriteLine("Wiggle-wiggle!");
+    }
+}
